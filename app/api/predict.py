32cb4692df2b3a3f4245baf53efbd369f1b3bdfb
StevenWestmoreland <stevenmwestmoreland@gmail.com>
2020-08-26 13:25:36 -0400

    deploy to Heroku

--- app/api/predict.py
+++ app/api/predict.py
@@ -4,52 +4,69 @@ import random
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+import spacy
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
 
 log = logging.getLogger(__name__)
 router = APIRouter()
+
+df = pd.read_csv('toking.csv')
+
+
+def search_func(user_input):
+
+    user_input = [user_input]
+    nlp=English()
+    tokenizer = Tokenizer(nlp.vocab)
+    tf = TfidfVectorizer(stop_words='english')
+    dtm = tf.fit_transform(df['search'])
+    dtm = pd.DataFrame(dtm.todense(), columns=tf.get_feature_names())
+
+    nn = NearestNeighbors(n_neighbors=5, algorithm='ball_tree')
+    nn.fit(dtm)
+    dtf = tf.transform(user_input)
+    _, output = nn.kneighbors(dtf.todense())
+
+
+    recommendations = []
+    for n in output:
+        for row in n:
+            recommendations.append(row)
+
+    result=[]
+    for i in recommendations:
+        data=(df.loc[i,:])
+        result.append(data)
+    return {'strain_recommendations': result}
 
 
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    symptom1: str = Field(..., example='insomnia')
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('x1')
-    def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
-        return value
-
 
 @router.post('/predict')
 async def predict(item: Item):
     """
-    Make random baseline predictions for classification problem 🔮
+    Make a baseline strain recommendation.
 
     ### Request Body
-    - `x1`: positive float
-    - `x2`: integer
-    - `x3`: string
+    - `symptom1`: string
+    - `symptom2`: string
+    - `symptom3`: string
 
     ### Response
-    - `prediction`: boolean, at random
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
-
-    Replace the placeholder docstring and fake predictions with your own model.
+    - `strain_recommendation`: list of strain names
     """
-
     X_new = item.to_df()
     log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
-    return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
-    }
+    return {'recommendations': search_func(item.symptom1)}
